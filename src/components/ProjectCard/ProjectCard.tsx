4d2b063de03df6311bf4f208e998b5b536e15297
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-08-23 18:30:48 -0700

    fix project cards and text

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -6,10 +6,9 @@ interface ProjectCardProps {
 
 const ProjectCard = ({title, description, technologies}: ProjectCardProps) => {
     return (
-        <div className="bg-white rounded-lg shadow-lg p-6">
+        <div className="bg-white dark:bg-gray-700 rounded-lg shadow-md p-6 dark:border dark:border-gray-600">
           <h3 className="text-xl font-bold mb-2">{title}</h3>
-          <p className="text-gray-600 mb-4">{description}</p>
-          <div className="flex flex-wrap">
+          <p className="text-gray-600 dark:text-gray-300 mb-4">{description}</p>          <div className="flex flex-wrap">
             {technologies.map((tech) => (
               <span key={tech} className="bg-gray-200 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded mb-2">
                 {tech}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -29,7 +29,7 @@ const Projects = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
             <ProjectCard key={project.title} {...project} />
-          ))}{" "}
+          ))}
         </div>
       </div>
     </section>
