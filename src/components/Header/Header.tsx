41c9f02ed42350f0d18f7c77b45c1c7fa4a1a67f
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-08-28 21:17:56 -0700

    add subtle animation when opening nav menu and its items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,6 +7,7 @@ import {
   faBars,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface HeaderProps {
   darkMode: boolean;
@@ -22,10 +23,9 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
 
   return (
     <header
-      className={`sticky top-0   
- w-full z-10 pt-4 transition-colors duration-300 ${
-   darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-800"
- }`}
+      className={`sticky top-0 w-full z-10 pt-4 transition-colors duration-300 ${
+        darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-800"
+      }`}
     >
       <div className="container mx-auto flex justify-between items-center p-4 relative">
         {/* Navigation links */}
@@ -34,101 +34,117 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
             isMenuOpen ? "block" : "hidden"
           }`}
         >
-          <ul
-            className={`flex flex-col md:flex-row md:space-x-4 md:mt-0 ${
-              darkMode ? "bg-gray-900" : "bg-white"
-            } md:bg-transparent p-4 `}
-          >
-            <li className={`relative ${activeLink === "hero" ? "active" : ""}`}>
-              <Link
-                to="hero"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
-                onClick={() => {
-                  setActiveLink("hero");
-                  toggleMenu();
-                }}
+          <AnimatePresence>
+            {isMenuOpen && (
+              <motion.ul
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.3 }}
+                className={`flex flex-col md:flex-row md:space-x-4 md:mt-0 ${
+                  darkMode ? "bg-gray-900" : "bg-white"
+                } md:bg-transparent p-4 `}
               >
-                Home
-                <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>{" "}
-              </Link>
-              {activeLink === "hero" && (
-                <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
-              )}
-            </li>
-            <li
-              className={`relative ${activeLink === "about" ? "active" : ""}`}
-            >
-              <Link
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
-                onClick={() => {
-                  setActiveLink("about");
-                  toggleMenu();
-                }}
-              >
-                About
-                <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>{" "}
-                {/* Underline effect */}
-              </Link>
-              {activeLink === "about" && (
-                <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
-              )}
-            </li>
-            <li
-              className={`relative ${
-                activeLink === "projects" ? "active" : ""
-              }`}
-            >
-              <Link
-                to="projects"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
-                onClick={() => {
-                  setActiveLink("projects");
-                  toggleMenu();
-                }}
-              >
-                Portfolio
-                <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>{" "}
-              </Link>
-              {activeLink === "projects" && (
-                <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
-              )}
-            </li>
-            <li
-              className={`relative ${activeLink === "contact" ? "active" : ""}`}
-            >
-              <Link
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
-                onClick={() => {
-                  setActiveLink("contact");
-                  toggleMenu();
-                }}
-              >
-                Contact
-                <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>{" "}
-              </Link>
-              {activeLink === "contact" && (
-                <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
-              )}
-            </li>
-          </ul>
+                <li
+                  className={`relative ${
+                    activeLink === "hero" ? "active" : ""
+                  }`}
+                >
+                  <Link
+                    to="hero"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
+                    onClick={() => {
+                      setActiveLink("hero");
+                      toggleMenu();
+                    }}
+                  >
+                    Home
+                    <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                  {activeLink === "hero" && (
+                    <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
+                  )}
+                </li>
+                <li
+                  className={`relative ${
+                    activeLink === "about" ? "active" : ""
+                  }`}
+                >
+                  <Link
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
+                    onClick={() => {
+                      setActiveLink("about");
+                      toggleMenu();
+                    }}
+                  >
+                    About
+                    <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>
+                    {/* Underline effect */}
+                  </Link>
+                  {activeLink === "about" && (
+                    <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
+                  )}
+                </li>
+                <li
+                  className={`relative ${
+                    activeLink === "projects" ? "active" : ""
+                  }`}
+                >
+                  <Link
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
+                    onClick={() => {
+                      setActiveLink("projects");
+                      toggleMenu();
+                    }}
+                  >
+                    Portfolio
+                    <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                  {activeLink === "projects" && (
+                    <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
+                  )}
+                </li>
+                <li
+                  className={`relative ${
+                    activeLink === "contact" ? "active" : ""
+                  }`}
+                >
+                  <Link
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className="block py-2 px-4 text-2xl font-bold text-gray-700 dark:text-gray-300 md:hover:bg-transparent rounded md:rounded-none transition duration-150 relative group"
+                    onClick={() => {
+                      setActiveLink("contact");
+                      toggleMenu();
+                    }}
+                  >
+                    Contact
+                    <span className="absolute bottom-0 left-0 w-0 h-1 bg-blue-500 rounded-full transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                  {activeLink === "contact" && (
+                    <div className="absolute bottom-0 left-0 w-full h-1 bg-blue-500 rounded-full"></div>
+                  )}
+                </li>
+              </motion.ul>
+            )}
+          </AnimatePresence>
         </nav>
         {/* Hamburger menu for mobile/tablet */}
         <div className="flex items-center">
